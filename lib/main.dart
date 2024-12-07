ec721ff70006fe38c863357d2e6e1ce5218b7bae
srisivasA <srisivas.a@technotackle.com>
2024-12-07 16:21:21 +0530

    scratchsetup

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:groceries/viewmodels/finance_viewmodel.dart';
+import 'package:groceries/views/finance_tracker_screen.dart';
+import 'package:provider/provider.dart';
+
+
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => FinanceTrackerViewModel()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: FinanceTrackerScreen(),
+      ),
+    ),
+  );
+}
